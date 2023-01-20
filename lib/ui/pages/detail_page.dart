f37ac58b479e5df35be75db5fe822e0788af07a4
Afiv Dicky Efendy <dickyareda@gmail.com>
2023-01-21 04:14:06 +0700

    mengintegrasikan dengan rest api

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,61 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hionepedia/services/http_service.dart';
 import 'package:hionepedia/theme/styles.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  const DetailPage({super.key, required this.animalData});
+  final dynamic animalData;
 
   @override
   Widget build(BuildContext context) {
     final audioPlayer = AudioPlayer();
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 20),
-              decoration: const BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
-              height: 300,
-              child: ModelViewer(
-                src:
-                    'https://hionepedia.000webhostapp.com/model/1674218229_cebbe33cf3dd824cd46b.glb',
-                alt: "A 3D model of an astronaut",
-                ar: true,
-                autoRotate: true,
-                cameraControls: true,
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(statusBarColor: Colors.amber),
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: const BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                height: 300,
+                child: ModelViewer(
+                  src: '${HttpService.apiUrl}/model/${animalData.model}',
+                  alt: '${animalData.name}',
+                  ar: true,
+                  autoRotate: true,
+                  cameraControls: true,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Astrounot',
-                    style: fontTeks,
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        audioPlayer.setUrl(
-                            'https://hionepedia.000webhostapp.com/sound/1674218229_4d06bdbd331687ad3dec.mp3');
-                        audioPlayer.play();
-                      },
-                      icon: Image.asset('assets/icons/sound.png'))
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '${animalData.name}',
+                      style: fontTeks.copyWith(fontSize: 24),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          audioPlayer.setUrl(
+                              '${HttpService.apiUrl}/sound/${animalData.sound}');
+                          audioPlayer.play();
+                        },
+                        icon: Image.asset('assets/icons/sound.png'))
+                  ],
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Text(
-                  'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'),
-            )
-          ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Text('${animalData.description}'),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:hionepedia/controllers/animal_controller.dart';
+import 'package:hionepedia/services/http_service.dart';
+import 'package:hionepedia/theme/styles.dart';
+import 'package:hionepedia/ui/pages/detail_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,17 +14,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final animalCtrl = Get.put(AnimalController());
     return Scaffold(
-      body: SafeArea(
-          child: Obx(() => animalCtrl.isLoading.isTrue
-              ? const CircularProgressIndicator()
-              : animalCtrl.animalData == null
-                  ? const Text('Data Kosong')
-                  : ListView.builder(
-                      itemCount: animalCtrl.animalData!.data.length,
-                      itemBuilder: (context, index) {
-                        return Text(animalCtrl.animalData!.data[index].name);
-                      },
-                    ))),
+      backgroundColor: whiteColor,
+      body: AnnotatedRegion(
+        value: SystemUiOverlayStyle(
+            statusBarColor: whiteColor,
+            statusBarIconBrightness: Brightness.dark),
+        child: SafeArea(
+          child: Obx(
+            () => animalCtrl.isLoading.isTrue
+                ? const Center(child: CircularProgressIndicator())
+                : animalCtrl.animalData == null
+                    ? const Text('Data Kosong')
+                    : GridView.custom(
+                        gridDelegate: SliverQuiltedGridDelegate(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4,
+                            pattern: [
+                              const QuiltedGridTile(4, 2),
+                              const QuiltedGridTile(2, 2),
+                              const QuiltedGridTile(2, 2),
+                              const QuiltedGridTile(2, 2),
+                              const QuiltedGridTile(2, 2),
+                            ]),
+                        childrenDelegate: SliverChildBuilderDelegate(
+                          childCount: animalCtrl.animalData!.data.length,
+                          (context, index) => GestureDetector(
+                            onTap: () => Get.to(DetailPage(
+                              animalData: animalCtrl.animalData!.data[index],
+                            )),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      '${HttpService.apiUrl}/thumbnail/${animalCtrl.animalData!.data[index].thumbnail}'),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+          ),
+        ),
+      ),
     );
   }
 }
