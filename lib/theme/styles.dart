328c8eb5e9e0e1bc4e64fc9dcb6a313c20800723
Afiv Dicky Efendy <dickyareda@gmail.com>
2023-06-14 01:36:45 +0700

    menambahkan google fonts

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color lightGrey = const Color(0xffEFEDED);
+Color primaryColor = const Color(0xff00416A);
+Color blackColor = const Color(0xff333333);
 
 List randomColor = [
   Colors.amber,
@@ -8,3 +11,7 @@ List randomColor = [
   Colors.pink,
   Colors.green,
 ];
+
+TextStyle titleStyle = GoogleFonts.poppins(fontSize: 20);
+TextStyle subtitleStyle = GoogleFonts.poppins(fontSize: 18);
+TextStyle textStyle = GoogleFonts.poppins(fontSize: 16);
